aacce006da06390e2d4be0cb6998ea82fe68b5f6
Silvan Biagi <silvan@Ordinateur-portable-de-Silvan.local>
2026-06-05 12:16:49 +0200

    Arrangement

---
+++ cours_devops/jour3/tests/test_app.py
@@ -0,0 +1,20 @@
+import pytest
+import json
+from app import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_accueil(client):
+    reponse = client.get('/')
+    assert reponse.status_code == 200
+    assert b'Bonjour' in reponse.data
+
+def test_sante(client):
+    reponse = client.get('/sante')
+    assert reponse.status_code == 200
+    data = json.loads(reponse.data)
+    assert data['statut'] == 'OK'
