e5c7bb82fad1a8f4f66bad95526f23a7d394a7a5
Silvan Biagi <silvan@OrdinatdeSilvan.lan>
2026-06-06 14:58:52 +0200

    MAJ 2

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def accueil():
-    return '<h1>Bonjour depuis Docker et Flask !</h1><p>Version 1.0</p>'
+    return f'<h1>Bonjour {nom} !</h1><p>Version 1.0</p>'
 
 
 @app.route('/sante')
