d9af61590649f39a64a6e538ca0f4ff256c65229
Silvan Biagi <silvan@Ordinateur-portable-de-Silvan.local>
2026-06-22 13:12:19 +0200

    Test

--- devportfolio/tests/test_routes.py
+++ devportfolio/tests/test_routes.py
@@ -1,3 +1,6 @@
+from flask_login import login_user
+
+
 def test_accueil(auth_client):
     r = auth_client.get('/')
     assert r.status_code == 200
@@ -7,9 +10,20 @@ def test_health_check(client):
     assert r.status_code == 200
     data = json.loads(r.data)
     assert data['statut'] == 'ok'
-def test_projet_inexistant(client):
-    r = client.get('/projet/9999')
-    assert r.status_code == 404
+
 def test_projet_existant(client, projet_test):
+    # Code pour te connecter ou simuler une session utilisateur ici
+    # Exemple si tu as un helper ou si tu utilises le client de test Flask-Login :
+    with client.session_transaction() as sess:
+        sess['_user_id'] = '1' # ID d'un utilisateur de test en BDD
+        sess['_fresh'] = True
+
     r = client.get(f'/projet/{projet_test}')
     assert r.status_code == 200
+def test_projet_inexistant(client):
+    with client.session_transaction() as sess:
+        sess['_user_id'] = '1'
+        sess['_fresh'] = True
+
+    r = client.get('/projet/9999')
+    assert r.status_code == 404
