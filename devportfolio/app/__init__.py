bbb01a29204d184e5af945d0625093da3adb9c19
Silvan Biagi <silvan@Ordinateur-portable-de-Silvan.local>
2026-06-22 12:29:43 +0200

    ?

--- devportfolio/app/__init__.py
+++ devportfolio/app/__init__.py
@@ -3,6 +3,9 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from dotenv import load_dotenv
+
+load_dotenv()
 
 try:
     from prometheus_flask_exporter import PrometheusMetrics
