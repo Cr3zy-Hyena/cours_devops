d90e262e10d167b15e3a013f6fd8672219675c7e
Silvan Biagi <silvan@Ordinateur-portable-de-Silvan.local>
2026-06-23 13:05:50 +0200

    reesaie

--- devportfolio/tests/test_api.py
+++ devportfolio/tests/test_api.py
@@ -1,30 +1,28 @@
 import json
 
-def test_lister_vide(client):
-    r = client.get('/api/projets')
+
+def test_lister_vide(auth_client):
+    r = auth_client.get('/api/projets')
     assert r.status_code == 200
     data = json.loads(r.data)
     assert data['total'] == 0
 
-def test_creer(client):
-    r = client.post('/api/projets', json=
-    {'titre':'Nouveau','description':'Desc','technologies':'Flask'})
+
+def test_creer(auth_client):
+    r = auth_client.post('/api/projets', json=
+    {'titre': 'Nouveau', 'description': 'Desc', 'technologies': 'Flask'})
     assert r.status_code == 201
     data = json.loads(r.data)
     assert data['titre'] == 'Nouveau'
 
-def test_creer_sans_titre(client):
-    r = client.post('/api/projets', json={'description':'Desc'})
+
+def test_creer_sans_titre(auth_client):
+    r = auth_client.post('/api/projets', json={'description': 'Desc'})
     assert r.status_code == 400
 
-def test_supprimer(client, projet_test):
-    r = client.delete(f'/api/projets/{projet_test}')
+
+def test_supprimer(auth_client, projet_test):
+    r = auth_client.delete(f'/api/projets/{projet_test}')
     assert r.status_code == 200
-    r2 = client.get(f'/api/projets/{projet_test}')
+    r2 = auth_client.get(f'/api/projets/{projet_test}')
     assert r2.status_code == 404
-    
-
-
-           
-
-    
